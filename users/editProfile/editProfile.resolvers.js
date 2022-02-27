ab1e81347f25dfd7fc3f1bddd3001f5c64b0efa7
Jim Kim <jim.hyunjin.kim@gmail.com>
2022-02-27 03:51:06 -0500

    Verify user token before editing profile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,19 +1,23 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import client from "../../client";
 
 export default {
   Mutation: {
     editProfile: async (
       _,
-      { firstName, lastName, username, email, password: newPassword }
+      { firstName, lastName, username, email, password: newPassword, token }
     ) => {
+      const { id } = await jwt.verify(token, process.env.SECRET_KEY);
+
       let encryptedPassword = null;
       if (newPassword) {
         encryptedPassword = await bcrypt.hash(newPassword, 10);
       }
+
       const updatedUser = await client.user.update({
         where: {
-          id: 1,
+          id,
         },
         data: {
           firstName,
