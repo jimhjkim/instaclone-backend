44420f29e36f32a12f253fef30c2522f027b6bd2
Jim Kim <jim.hyunjin.kim@gmail.com>
2022-02-27 00:27:05 -0500

    Add update profile logic to editProfile mutation

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,38 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi"),
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword }
+    ) => {
+      let encryptedPassword = null;
+      if (newPassword) {
+        encryptedPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updatedUser = await client.user.update({
+        where: {
+          id: 1,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(encryptedPassword && { password: encryptedPassword }),
+        },
+      });
+      if (updatedUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update profile",
+        };
+      }
+    },
   },
 };
